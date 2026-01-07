e3fda085d527c08b94fe886896a7acd175a591d7
Eduardo Mury Assafin <101987520+Dudzz01@users.noreply.github.com>
2026-01-07 18:36:42 -0300

    feat new fields in item entity

--- Assets/Scripts/Models/Item.cs
+++ Assets/Scripts/Models/Item.cs
@@ -12,6 +12,14 @@ namespace Models
         public string Descricao { get; set; }
 
         public string Dica { get; set; }
+
+        public string Tipo { get; set; } = "ESTOQUE";   
+        public int Quantidade { get; set; } = 0;
+        public string Unidade { get; set; } = "un";
+        public string ValidoAte { get; set; }          
+
+        public string Status { get; set; } = "OK";     
+        public int Prioridade { get; set; } = 3;
     }
 
 }
